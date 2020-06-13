a0cb20c29d7a46c18ca2a13dda81c90a285e2aad
sm4rtk1dz <sm4rtk1dz@protonmail.com>
2020-06-13 03:09:47 -0300

    little refactor

--- install.sh
+++ install.sh
@@ -15,8 +15,8 @@ prompt_install() {
 	while true; do
 	    read -p "Do you want to install ${1}? [Y/n] " yn
 	    case $yn in
-		[Yy]* ) $2; break;;
-	    [Nn]* ) echo "Skipping ${1}"; break;;
+			[Yy]* ) $2; break;;
+		    [Nn]* ) break;;
 	    * ) echo "Please answer yes or no.";;
 	    esac
 	done
@@ -24,7 +24,8 @@ prompt_install() {
 
 install_debian_tools() {
 	sudo apt install -y php golang
-	sudo apt install -y ufw net-tools strace ltrace gobuster
+	sudo apt install -y ufw net-tools gobuster
+	sudo apt install -y strace ltrace
 	sudo apt install -y steghide
 	sudo apt autoremove -y
 }
@@ -37,7 +38,7 @@ install_git_repos() {
 prompt_install "git repos" install_git_repos
 
 install_ffuf() {
-	wget -q -L github.com/ffuf/ffuf/releases/download/v1.0.2/ffuf_1.0.2_linux_amd64.tar.gz
+	wget -L github.com/ffuf/ffuf/releases/download/v1.0.2/ffuf_1.0.2_linux_amd64.tar.gz
 	mkdir ffuf 
 	tar -xzf ffuf_1.0.2_linux_amd64.tar.gz -C ffuf
 	rm ffuf_1.0.2_linux_amd64.tar.gz
