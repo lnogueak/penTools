608f6f9d2736a641ede81e9f3eb80e4b1f781413
sm4rtk1dz <sm4rtk1dz@protonmail.com>
2020-08-29 23:28:06 -0400

    upgraded installer

--- install.sh
+++ install.sh
@@ -11,7 +11,16 @@ echo "
 Penetration testing tools installer for Debian [by Sm4rtK1dz]
 "
 
-install_debian_tools() {
+help_menu() {
+	echo "
+	-h	help menu
+	-d	install apt-get installables
+	-g	clone git repos
+	-e	clone experimental repos
+	"
+}
+
+install_debian() {
 	sudo apt update
 	# Programming Languages
 	sudo apt install -y php golang python-pip python3-pip
@@ -22,7 +31,14 @@ install_debian_tools() {
 	sudo apt autoremove -y
 }
 
-install_git_repos() {
+install_ffuf() {
+	wget -L github.com/ffuf/ffuf/releases/download/v1.0.2/ffuf_1.0.2_linux_amd64.tar.gz
+	mkdir ./enum/ffuf 
+	tar -xzf ffuf_1.0.2_linux_amd64.tar.gz -C ./enum/ffuf
+	rm ffuf_1.0.2_linux_amd64.tar.gz
+}
+
+install_git() {
 	git clone https://github.com/rebootuser/LinEnum.git ./enum/LinEnum
 	git clone https://github.com/carlospolop/privilege-escalation-awesome-scripts-suite.git ./enum/PEASS
 	git clone https://github.com/pentestmonkey/php-reverse-shell.git ./shell/php-reverse-shell
@@ -30,21 +46,35 @@ install_git_repos() {
 	git clone https://github.com/Anon-Exploiter/SUID3NUM ./enum/SUID3NUM
 	git clone https://github.com/trickster0/Enyx.git ./enum/ipv6Enyx
 
-	wget -P ./enum https://github.com/DominicBreuker/pspy/releases/download/v1.2.0/pspy64
-	wget -P ./enum https://github.com/DominicBreuker/pspy/releases/download/v1.2.0/pspy32
-}
-	
-install_ffuf() {
-	wget -L github.com/ffuf/ffuf/releases/download/v1.0.2/ffuf_1.0.2_linux_amd64.tar.gz
-	mkdir ffuf 
-	tar -xzf ffuf_1.0.2_linux_amd64.tar.gz -C ffuf
-	rm ffuf_1.0.2_linux_amd64.tar.gz
-	mv ffuf enum/ffuf
+	[ ! -f ./enum/pspy64 ] && wget -P ./enum https://github.com/DominicBreuker/pspy/releases/download/v1.2.0/pspy64
+	[ ! -f ./enum/pspy32 ] && wget -P ./enum https://github.com/DominicBreuker/pspy/releases/download/v1.2.0/pspy32
+
+	[ ! -d ./enum/ffuf ] install_ffuf
 }
 
-install_debian_tools
-install_git_repos
-install_ffuf
+install_experimental() {
+	git clone https://github.com/SafeBreach-Labs/SirepRAT.git ./experimental/SirepRAT
+	git clone https://github.com/OWASP/D4N155 ./experimental/D4N155
+}
 
-## Test Line
+#install_debian_tools
+#install_git_repos
+
+while getopts hdge option
+do
+	case "${option}"
+	in
+	h) help_menu;;
+	d) install_debian;;
+	g) install_git;;
+	e) install_experimental;;
+	*) help_menu;;
+	esac
+done
+
+if [ $# -eq 0 ]
+  then
+    help_menu
+fi
+
 exit 1
